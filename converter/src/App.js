8e65db930a7ed34321d7093e2e5c38ab28b46bda
jubinjacob03 <jr.jubin03@gmail.com>
2024-03-17 01:12:23 +0530

    Major UI changes

--- converter/src/App.js
+++ converter/src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import urlShortenerImg from './banner-main-alt.png';
-import urlicon from './url-icon.png';
-import clipart1 from './clipart1.png';
-import clipart2 from './clipart2.png';
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+import urlShortenerImg from "./banner-main-alt.png";
+import urlicon from "./url-icon.png";
+import clipart1 from "./clipart1.png";
+import clipart2 from "./clipart2.png";
+import card1 from "./card1.png";
+import card2 from "./card2.png";
+import Box from "@mui/material/Box";
+import "./App.css";
+import "./App.css";
 
 function UrlShortener() {
-  const [originalUrl, setOriginalUrl] = useState('');
+  const [originalUrl, setOriginalUrl] = useState("");
   const [shortenedUrl, setShortenedUrl] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false); // State to manage alert visibility
+  const [alertMessage, setAlertMessage] = useState(""); // State to manage alert message
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,80 +25,104 @@ function UrlShortener() {
 
     try {
       const response = await axios.post(
-        'https://ipjesjo0va.execute-api.ap-southeast-2.amazonaws.com/url-shortner',
-         originalUrl , // Sending the original URL as part of the request body
+        "https://ipjesjo0va.execute-api.ap-southeast-2.amazonaws.com/url-shortner",
+        originalUrl, // Sending the original URL as part of the request body
         {
           headers: {
-            'Content-Type': 'text/plain', // Set content type to application/json
+            "Content-Type": "text/plain", // Set content type to application/json
           },
         }
       );
       setShortenedUrl(response.data.shortenedUrl); // Assuming the shortened URL is in the response data
     } catch (error) {
       console.error(error);
-      setError('Error shortening URL');
+      setError("Error shortening URL");
     } finally {
       setIsLoading(false);
     }
   };
 
+  const handleAboutClick = () => {
+    setAlertMessage("This project leverages AWS Lambda, a serverless computing service, for seamless integration of frontend with backend functionality. By utilizing AWS Lambda and TinyURL PHP it efficiently shorten URLs, optimizing resource usage and enhancing user experience. Overall this project was a work to understand AWS Lambda serverless computing and how its integration works with frontend :)");
+     // Set alert message
+    setShowAlert(true); // Show alert
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  };
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh', backgroundColor: '#f8f9fa' }}>
-      <header style={{ marginBottom: '40px',display:'flex',flexDirection:'column',gap:'16px' }}>
-      <img src={urlShortenerImg} alt="URL Shortener" style={{ maxWidth: '35vw', height: 'auto' }} />
-        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <>
+         {showAlert && (
+        <div className="alert">
+          <span>{alertMessage}</span>
+        </div>
+      )}
+    <header>
+      <a href="#" class="logo">
+        SHORTIFY
+      </a>
+      <ul class="navlist">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#" onClick={handleAboutClick}>About</a>
+        </li>
+        <li>
+          <a href="#">Github</a>
+        </li>
+        <li>
+          <a href="#">Contact Me</a>
+        </li>
+      </ul>
+      <div class="ri-amazon-line" id="menu-icon"></div>
+    </header>
+    <section class="hero">
+      <div className="hero-text">
+        <h5>Free URL Shortener</h5>
+        <h4>Shorten any url on the go <i class="ri-flashlight-line"></i></h4>
+        <h1>URL Shortener</h1>
+        <p style={{letterSpacing:'1.25px'}}>This project seamlessly merges AWS Lambda with TinyURL PHP, offering a dynamic solution for swiftly shortening URLs tailored to your requirements.</p>
+        <form onSubmit={handleSubmit} >
           <input
             type="text"
             id="url"
             value={originalUrl}
             onChange={(e) => setOriginalUrl(e.target.value)}
-            style={{ width: '80%', marginBottom: '20px', padding: '10px', fontFamily: 'Rubik, sans-serif', fontSize: '1em', border: '2px solid #ccc', borderRadius: '5px', transition: 'border-color 0.3s', textAlign: 'center',
+            style={{ width: '80%', marginBottom: '20px', padding: '10px', fontSize: '1em', border: '2px solid #ccc', borderRadius: '5px', transition: 'border-color 0.3s', textAlign: 'center',
            }}
-            placeholder="Enter your URL here"
+            placeholder="Paste URL here"
             required
           />
-          <button type="submit" disabled={isLoading} style={{ width: '50%', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '15px', cursor: 'pointer', fontFamily: 'Rubik, sans-serif', fontSize: '1em', transition: 'background-color 0.3s' }}>
+          <button type="submit" disabled={isLoading} >
             {isLoading ? 'Shortening...' : 'Shorten URL'}
           </button>
-        </form>
-        {shortenedUrl && (
-          <div style={{ backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '5px', marginTop: '40px', animation: 'fadeIn 1s' }}>
-            <p style={{ margin: 0, fontFamily: 'Rubik, sans-serif', fontSize: '1.2em', color: '#333', textAlign: 'center' }}><strong>Shortened URL:</strong></p>
-            <a href={shortenedUrl} target="_blank" rel="noopener noreferrer" style={{ color: '#007bff', wordBreak: 'break-all', fontFamily: 'Rubik, sans-serif', fontSize: '1.2em', textDecoration: 'none', transition: 'color 0.3s', display: 'block', marginTop: '10px', textAlign: 'center' }}>{shortenedUrl}</a>
+      <a href="#" class="ctaa"><i class="ri-external-link-line"></i>Learn More</a>
+      </form>
+      </div>
+
+      <div className="hero-img">
+        <img src={urlShortenerImg} alt="URL Shortener" />
+      </div>
+      {shortenedUrl && (
+          <div style={{ animation: 'fadeIn 1s' }}>
+            <p  class="result"><strong><i class="ri-link"></i> Shortened URL : </strong><a className="result-url" href={shortenedUrl} target="_blank" rel="noopener noreferrer" >{shortenedUrl}</a></p>
           </div>
         )}
-        {error && <p style={{ color: 'red', fontFamily: 'Rubik, sans-serif', fontSize: '1.2em', marginTop: '20px', textAlign: 'center' }}>{error}</p>}
-      </header>
-      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '40px' }}>
-
-        {/* <img src={clipart1} alt="Clipart" style={{ maxWidth: '200px', height: 'auto' }} />
-        <img src={clipart2} alt="Clipart" style={{ maxWidth: '200px', height: 'auto' }} /> */}
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '40px',gap:'30px' }}>
-        <div className="card" style={{ width: '300px', textAlign: 'center', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', cursor: 'pointer', transition: 'transform 0.3s' }}>
-          <h3>Why Choose Us?</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="card" style={{ width: '300px', textAlign: 'center', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', cursor: 'pointer', transition: 'transform 0.3s' }}>
-          <h3>Our Mission</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="card" style={{ width: '300px', textAlign: 'center', padding: '20px', backgroundColor: '#fff', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', cursor: 'pointer', transition: 'transform 0.3s' }}>
-          <h3>Customer Satisfaction</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <footer style={{ marginTop: 'auto', textAlign: 'center', padding: '20px', backgroundColor: '#343a40', color: '#fff', width: '100%' }}>
-        <p style={{ margin: 0, fontFamily: 'Rubik, sans-serif', fontSize: '1em' }}>© 2024 URL Shortener. All rights reserved.</p>
-      </footer>
-      <style>
-        {`
-          .card:hover {
-            transform: scale(1.05);
-          }
-        `}
-      </style>
+        {error && <p style={{ color: '#f7f30c',fontSize:'20px', textAlign: 'center' }}><i class="ri-alert-line"></i> {error}</p>}
+    </section>
+    <div class="icons">
+      <a href="#"><i class="ri-github-line"></i></a>
+      <a href="#"><i class="ri-cloud-line"></i></a>
+      <a href="#"><i class="ri-function-line"></i></a>
     </div>
+    <div class="scroll-down">
+      <a href="#">
+        <i class="ri-arrow-down-s-line"></i>
+      </a>
+    </div>
+    </>
   );
 }
 
