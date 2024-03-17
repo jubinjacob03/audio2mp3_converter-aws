a69d383bc76e82ee7f78170b37ded4b706ef97c5
jubinjacob03 <jr.jubin03@gmail.com>
2024-03-17 20:01:19 +0530

    Major Frontend changes

--- converter/src/App.js
+++ converter/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import urlShortenerImg from "./banner-main-alt.png";
-import "./App.css";
+import banner_image from "./banner-main-alt.png";
 import "./App.css";
 
 function UrlShortener() {
@@ -9,6 +8,7 @@ function UrlShortener() {
   const [shortenedUrl, setShortenedUrl] = useState(null);  // State to store the shortened URL
   const [isLoading, setIsLoading] = useState(false);  // State to store the loading state
   const [error, setError] = useState(null);  // State to store the error
+
 
   const handleSubmit = async (e) => {  // Function to handle form submission
     e.preventDefault();  // Prevent the default form submission
@@ -34,6 +34,11 @@ function UrlShortener() {
     }
   };
 
+  const handleHomeClick = () => {
+    window.location.reload(); // Reload the site
+    setOriginalUrl(""); // Reset the input box
+  };
+
 
   return (
     <>
@@ -42,20 +47,30 @@ function UrlShortener() {
         SHORTIFY
       </a>
       <ul className="navlist">
-        <li>
-          <a href="#">Home</a>
+        <li className="navlist-text">
+          <a onClick={handleHomeClick}>Home</a>
         </li>
-        <li>
+        <li className="navlist-text">
           <a href="#">Github</a>
         </li>
-        <li>
+        <li className="navlist-text">
           <a href="#">Contact Me</a>
         </li>
+        <li className="navlist-icon">
+            <a onClick={handleHomeClick}><i className="ri-home-line"></i></a>
+          </li>
+          <li className="navlist-icon">
+            <a href="#"><i className="ri-github-line"></i></a>
+          </li>
+          <li className="navlist-icon">
+            <a href="#"><i className="ri-user-line"></i></a>
+          </li >
       </ul>
-      <div class="ri-amazon-line" id="menu-icon"></div>
+      <a href="https://aws.amazon.com/getting-started/" class="ri-amazon-line" id="menu-icon"></a>
     </header>
-    <section className="hero">
-      <div className="hero-text">
+
+    <section className="banner">
+      <div className="banner-text">
         <h4>Shorten any url on the go <i className="ri-flashlight-line"></i></h4>
         <h1>URL Shortener</h1>
         <p className="desc">This project seamlessly merges AWS Lambda with TinyURL PHP, offering a dynamic solution for swiftly shortening URLs tailored to your requirements.</p>
@@ -69,27 +84,27 @@ function UrlShortener() {
             placeholder="Paste URL here"
             required
           />
-          <button type="submit" disabled={isLoading} >
+          <button  className="shorten-button" type="submit" disabled={isLoading} >
             {isLoading ? 'Shortening...' : 'Shorten URL'}
           </button>
       <a href="#" className="ctaa"><i class="ri-external-link-line"></i>Learn More</a>
       </form>
       </div>
 
-      <div className="hero-img">
-        <img src={urlShortenerImg} alt="URL Shortener" />
+      <div className="banner-img">
+        <img src={banner_image} alt="SHORTIFY-banner" />
       </div>
       {shortenedUrl && (
           <div style={{ animation: 'fadeIn 1s' }}>
             <p  className="result"><strong><i class="ri-link"></i> Shortened URL : </strong><a className="result-url" href={shortenedUrl} target="_blank" rel="noopener noreferrer" >{shortenedUrl}</a></p>
           </div>
         )}
-        {error && <p style={{ color: '#f7f30c',fontSize:'20px', textAlign: 'center' }}><i class="ri-alert-line"></i> {error}</p>}
+        {error && <p className="result" style={{ color: '#f7f30c',fontSize:'20px', textAlign: 'center' }}><i class="ri-alert-line"></i> {error}</p>}
     </section>
     <div class="icons">
       <a href="#"><i class="ri-github-line"></i></a>
-      <a href="#"><i class="ri-cloud-line"></i></a>
-      <a href="#"><i class="ri-function-line"></i></a>
+      <a href="https://aws.amazon.com/lambda/"><i class="ri-cloud-line"></i></a>
+      <a href="https://tinyurl.com/app/features/how-tinyurl-works"><i class="ri-function-line"></i></a>
     </div>
     </>
   );
